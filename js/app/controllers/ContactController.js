181135696d34f9ab37ffa241763ff3c7b3e23d91
babyshoes <dianecai@gmail.com>
2016-04-03 15:18:36 -0400

    done

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Diane';
+
+	$timeout(function() {
+		$scope.name = 'a different name';
+	}, 5000)
 }
 
 angular
